93602e0e2add7c99b605623c4aa96180c77c8408
Vuk Uskokovic <uskokposlovni@gmail.com>
2023-04-07 11:36:14 +0200

    Exposed files with api key which can be set via an enviroment variable

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import bodyParser from 'body-parser'
 import {readdir} from 'fs/promises'
+import {existsSync} from 'fs'
 import { logsFolder } from "./sessionLoggin";
+import path from 'path'
 const router = Router()
 const jsonParser = bodyParser.json()
 
@@ -9,7 +11,6 @@ const API_KEY = process.env.API_KEY || "defaultApiKey"
 
 router.use(jsonParser, async(req, res, next) => {
     const {key} = req.body
-    console.log(key, API_KEY)
     if(key !== API_KEY){
         res.statusCode = 401
         res.statusMessage = 'Api key does not match'
@@ -22,10 +23,35 @@ router.use(jsonParser, async(req, res, next) => {
 router.get("/folders", async (_, res) => {
     try{
         const dirs = await readdir(logsFolder)
-    res.json(dirs)
+        res.json(dirs)
     }catch{
         res.statusCode = 500
         res.json("Error listing directories")
+    }
+})
+
+router.get("/folder/:folderName", async (req, res) => {
+    try{
+        const files = await readdir(path.join(logsFolder, req.params.folderName))
+        res.json(files)
+    }catch{
+        res.statusCode = 500
+        res.json("Error listing files")
+    }
+})
+
+router.get("/file/:folderName/:file", async (req, res) => {
+    try{
+        const filePath = path.join(logsFolder, req.params.folderName, req.params.file)
+        if(!existsSync(filePath)){
+            res.statusCode = 404
+            res.json("File not found")
+            return;
+        }
+        res.sendFile(filePath)
+    }catch{
+        res.statusCode = 500
+        res.json("Error getting file")
     }
 })
 
